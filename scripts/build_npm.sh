1b8b86f0c4aa6df1843e94494b873ecd3ba2f625
Tim Supinie <tsupinie@gmail.com>
2023-11-03 19:32:49 -0500

    Fix @s in the build script

--- scripts/build_npm.sh
+++ scripts/build_npm.sh
@@ -28,10 +28,10 @@ do
         filename=`echo $imp | sed 's/.*"\(.*\.glsl\)".*/\1/' | sed "s/.*'\(.*\.glsl\)'.*/\1/"`
         variable=`echo $imp | tr -s ' ' | sed 's/const \(.*\) =.*/\1/' | sed 's/let \(.*\) =.*/\1/' | sed 's/var \(.*\) =.*/\1/'`
         
-        file_contents=`cat lib/$filename | sed "s%//.*%%" | tr '&' '@' | tr '\n' '^'`
+        file_contents=`cat lib/$filename | sed "s%//.*%%" | sed 's%&%\\\&%g' | tr '\n' '^'`
         replacement="const $variable = \`$file_contents\`"
 
-        cat /tmp/file.js | sed "s%$imp%$replacement%" | tr '@' '&' | tr '^' '\n' > /tmp/file.out.js
+        cat /tmp/file.js | sed "s%$imp%$replacement%" | tr '^' '\n' > /tmp/file.out.js
         mv /tmp/file.out.js /tmp/file.js
     done
 
