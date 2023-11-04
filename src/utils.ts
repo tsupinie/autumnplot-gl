b322d62105019455bef96c838fe7d0be739827b6
Tim Supinie <tsupinie@gmail.com>
2023-11-04 00:04:50 -0500

    Code style tweaks to getOS()

--- src/utils.ts
+++ src/utils.ts
@@ -120,26 +120,30 @@ function* zip(...args: any[]) {
 }
 
 function getOS() {
-  var userAgent = window.navigator.userAgent,
-      platform = window.navigator.platform,
-      macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
-      windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
-      iosPlatforms = ['iPhone', 'iPad', 'iPod'],
-      os = null;
+    const userAgent = window.navigator.userAgent,
+        platform = window.navigator.platform,
+        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'];
+    let os = null;
 
-  if ((macosPlatforms.indexOf(platform) !== -1) && (navigator.maxTouchPoints <= 1)) {
-    os = 'Mac OS';
-  } else if ((iosPlatforms.indexOf(platform) !== -1)  || ( (macosPlatforms.indexOf(platform) !== -1)  && (navigator.maxTouchPoints > 1))) { 
-    os = 'iOS';
-  } else if (windowsPlatforms.indexOf(platform) !== -1) {
-    os = 'Windows';
-  } else if (/Android/.test(userAgent)) {
-    os = 'Android';
-  } else if (/Linux/.test(platform)) {
-    os = 'Linux';
-  }
+    if (macosPlatforms.indexOf(platform) !== -1 && navigator.maxTouchPoints <= 1) {
+        os = 'Mac OS';
+    } 
+    else if (iosPlatforms.indexOf(platform) !== -1 || (macosPlatforms.indexOf(platform) !== -1 && navigator.maxTouchPoints > 1)) { 
+        os = 'iOS';
+    } 
+    else if (windowsPlatforms.indexOf(platform) !== -1) {
+        os = 'Windows';
+    } 
+    else if (/Android/.test(userAgent)) {
+        os = 'Android';
+    } 
+    else if (/Linux/.test(platform)) {
+        os = 'Linux';
+    }
 
-  return os;
+    return os;
 }
 
 export {hex2rgba, rgba2hex, hex2rgb, rgb2hex, rgb2hsv, hsv2rgb, zip, getMinZoom, getOS};
