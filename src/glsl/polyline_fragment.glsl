99d719b272f645114b2a58cb5e01c35d47117d45
Tim Supinie <tsupinie@gmail.com>
2024-07-02 20:07:48 -0500

    Use the floor(zoom) to modify dash size instead of just zoom.

--- src/glsl/polyline_fragment.glsl
+++ src/glsl/polyline_fragment.glsl
@@ -15,7 +15,7 @@ varying highp float v_data;
 varying highp float v_dist;
 
 void main() {
-    lowp float dash_x = fract(v_dist * 2e2 * pow(2., u_zoom) / float(u_dash_pattern_length));
+    lowp float dash_x = fract(v_dist * 2e2 * pow(2., floor(u_zoom)) / float(u_dash_pattern_length));
     lowp float dash = texture2D(u_dash_sampler, vec2(dash_x, 0.5)).r;
 
     lowp vec4 color;
