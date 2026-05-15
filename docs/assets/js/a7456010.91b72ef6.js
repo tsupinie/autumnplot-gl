8a72c6e6559bc20822bba4d678eb72ece4376cf4
Tim Supinie (Docusaurus Deploy Bot) <tsupinie@gmail.com>
2026-05-15 14:27:59 +0000

    Update docs to v1.1.0

---
+++ docs/assets/js/a7456010.91b72ef6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkautumnplot_gl_docs=self.webpackChunkautumnplot_gl_docs||[]).push([[1235],{8552:u=>{u.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
